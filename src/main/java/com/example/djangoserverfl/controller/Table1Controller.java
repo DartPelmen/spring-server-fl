d51e4c328080c55845c5a0e140bb4e4eccc1c613
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2022-09-18 17:26:01 +1000

    aw

--- src/main/java/com/example/djangoserverfl/controller/Table1Controller.java
+++ src/main/java/com/example/djangoserverfl/controller/Table1Controller.java
@@ -5,6 +5,7 @@ import com.example.djangoserverfl.model.Table1Model;
 import com.example.djangoserverfl.service.Table1Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +21,7 @@ public class Table1Controller {
         return table1Service.getData();
     }
     @PostMapping("/")
-    public Status insertData(Table1Model table1Model){
+    public Status insertData(@RequestBody  Table1Model table1Model){
         return table1Service.insertData(table1Model);
     }
 }
